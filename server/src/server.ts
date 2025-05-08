c37de8cb2493213178a569b8de161eac77efec05
pieroBri <pierobri36@gmail.com>
2025-05-08 15:54:03 -0400

    cambios pruebas deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { Server, Socket } from 'socket.io';
 import manejoReservas from './controllers/conections';
 
 const app = express();
-const PORT = 4000;
+//const PORT = 4000;
 
 const server = http.createServer(app);
 
@@ -14,6 +14,7 @@ app.use(cors());
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
   },
 });
 
@@ -30,12 +31,11 @@ io.on('connection', (socket: Socket) => {
   });
 });
 
-app.get('/api', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello world',
-  });
+app.get("/", (req, res) => {
+  res.send("Express + Socket.io Server Running");
 });
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
